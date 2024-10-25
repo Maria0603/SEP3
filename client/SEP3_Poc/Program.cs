41806473045eee6a056709ec1c416da1f4545275
Mariia Moskovko <masha.moskovko@gmail.com>
2024-10-25 11:14:10 +0200

    c# init

---
+++ client/SEP3_Poc/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SEP3_Poc;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// Configure HttpClient for API calls
+builder.Services.AddScoped(sp => new HttpClient
+    { BaseAddress = new Uri("http://localhost:8080/") });
+
+await builder.Build().RunAsync();
+
+
