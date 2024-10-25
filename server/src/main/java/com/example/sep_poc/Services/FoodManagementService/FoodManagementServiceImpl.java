50ee45158d77e8f75005e84fec7a7328469b74e2
betelgeuseBet <151634373+betelgeuseBet@users.noreply.github.com>
2024-10-25 15:05:39 +0200

    Refined the system - deleted default pages, applied Bootstrap and responsiveness

--- server/src/main/java/com/example/sep_poc/Services/FoodManagementService/FoodManagementServiceImpl.java
+++ server/src/main/java/com/example/sep_poc/Services/FoodManagementService/FoodManagementServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import proto.FoodBag.*;
-
 import proto.FoodManagementServiceGrpc;
 
 @Service public class FoodManagementServiceImpl
