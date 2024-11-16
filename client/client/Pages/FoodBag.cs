751aa070225ad48beeaf0030fe590433effeb132
Jan Kalina <teksas77@gmail.com>
2024-11-16 13:27:44 +0100

    Add new client folder

---
+++ client/client/Pages/FoodBag.cs
@@ -0,0 +1,14 @@
+namespace client.Pages
+{
+    public class FoodBag
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public short Price { get; set; }
+        public string Address { get; set; }
+        public string PickupTime1 { get; set; }
+        public string PickupTime2 { get; set; }
+        public string PickupDate { get; set; }
+        public string Collector { get; set; }
+    }
+}
