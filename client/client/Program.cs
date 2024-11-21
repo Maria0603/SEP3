dce9574a54c81271333a79f633f6e51b00a9883a
AleksandraVIA <293687@via.dk>
2024-11-21 10:58:15 +0100

    finishing offer-ui

--- client/client/Program.cs
+++ client/client/Program.cs
@@ -1,14 +1,12 @@
+using client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
 // Configure HttpClient for API calls
-builder.Services.AddScoped(sp => new HttpClient
-    { BaseAddress = new Uri("http://localhost:8082/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
