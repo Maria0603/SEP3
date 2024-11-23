a53499e4642aebd6dd05fcc3d5f777bf346e6e8d
FlorinaMt <florina.mt77@gmail.com>
2024-11-23 01:38:58 +0100

    Data transfer objects reviewed

--- client/client/DTO/CreateOfferRequestDto.cs
+++ client/client/DTO/CreateOfferRequestDto.cs
@@ -1,17 +1,15 @@
-﻿using client.Pages;
-
-namespace client.DTO;
+﻿namespace client.DTO;
 
 public class CreateOfferRequestDto
 {
-    public string? Title { get; set; }
-    public string? Description { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
     public int OriginalPrice { get; set; }
     public int OfferPrice { get; set; }
     public int NumberOfItems { get; set; }
-    public PickupDate? PickupDate { get; set; }
-    public PickupTime? PickupTimeStart { get; set; }
-    public PickupTime? PickupTimeEnd { get; set; }
+    public DateDto PickupDate { get; set; }
+    public TimeDto PickupTimeStart { get; set; }
+    public TimeDto PickupTimeEnd { get; set; }
     public List<string>? Categories { get; set; }
     public byte[]? Image { get; set; }
 }
