7c7adbc979a1631bdd458c5dda6563d4984e396c
Jan Kalina <teksas77@gmail.com>
2024-11-28 10:31:08 +0100

    CORS allow all origin for development

--- server/src/main/java/com/example/server/config/WebConfig.java
+++ server/src/main/java/com/example/server/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:7047")
+//        .allowedOrigins("http://localhost:7047")
+         .allowedOriginPatterns("*")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true);
