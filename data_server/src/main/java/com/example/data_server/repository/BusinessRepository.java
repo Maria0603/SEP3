788e37f1068f5cb4184fb3f39421ece127a87a5e
FlorinaMt <florina.mt77@gmail.com>
2024-11-30 00:46:02 +0100

    Repositories for accessing businesses database collection

--- data_server/src/main/java/com/example/data_server/repository/BusinessRepository.java
+++ data_server/src/main/java/com/example/data_server/repository/BusinessRepository.java
@@ -1,10 +1,12 @@
 package com.example.data_server.repository;
 
-import com.example.shared.dao.BusinessDao;
-import com.example.shared.dao.OfferDao;
+import com.example.shared.dao.usersDao.BusinessDao;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
 
 public interface BusinessRepository extends MongoRepository<BusinessDao, String>
 {
-
+  Optional<BusinessDao> findByEmail(String email);
+  void deleteByEmail(String email);
 }
