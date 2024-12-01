ca631c827231f52f3c85306ada1a0205e6e0d2f9
AleksandraVIA <293687@via.dk>
2024-12-01 16:12:05 +0100

    Dummy business profile + log out done

---
+++ client/client/DTO/BusinessProfileDto.cs
@@ -0,0 +1,9 @@
+namespace client.DTO;
+
+public class BusinessProfileDto
+{
+    public string BusinessName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public byte[] Image { get; set; }
+}
