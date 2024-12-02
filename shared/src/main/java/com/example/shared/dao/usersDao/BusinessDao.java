ae5f9238dc297cfd18c52b629df504d0fe4ae7f6
FlorinaMt <florina.mt77@gmail.com>
2024-12-02 09:54:33 +0100

    Divided user logic into business and customer

--- shared/src/main/java/com/example/shared/dao/usersDao/BusinessDao.java
+++ shared/src/main/java/com/example/shared/dao/usersDao/BusinessDao.java
@@ -1,15 +1,110 @@
 package com.example.shared.dao.usersDao;
 
 import com.example.shared.dao.auxDao.AddressDao;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-@Document(collection = "businesses") public class BusinessDao extends UserDao
+import java.util.Collection;
+import java.util.List;
+
+@Document(collection = "businesses") public class BusinessDao
+    implements UserDetails
 {
+  @Id private String id;
+  @Field("email") @Indexed(unique = true) private String email;
+  @Field("phone_number") private String phoneNumber;
+  @Field("hashed_password") private String password;
+  @Field("role") private String role; // "CUSTOMER" or "BUSINESS"
   @Field("business_name") private String businessName;
   @Field("cvr") private String cvr;
   @Field("logo_path") private String logoPath;
   @Field("address") private AddressDao address;
+
+  // the email will be used to log in, not the username, but the method must be called getUsername
+  @Override public String getUsername()
+  {
+    return email;
+  }
+
+  @Override public Collection<? extends GrantedAuthority> getAuthorities()
+  {
+    return List.of(new SimpleGrantedAuthority(role));
+  }
+
+  @Override public boolean isAccountNonExpired()
+  {
+    return true;
+  }
+
+  @Override public boolean isAccountNonLocked()
+  {
+    return true;
+  }
+
+  @Override public boolean isCredentialsNonExpired()
+  {
+    return true;
+  }
+
+  @Override public boolean isEnabled()
+  {
+    return true;
+  }
+
+  public String getPhoneNumber()
+  {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber)
+  {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getRole()
+  {
+    return role;
+  }
+
+  public void setRole(String role)
+  {
+    this.role = role;
+  }
+
+  public String getPassword()
+  {
+    return password;
+  }
+
+  public void setPassword(String password)
+  {
+    this.password = password;
+  }
+
+  public String getEmail()
+  {
+    return email;
+  }
+
+  public void setEmail(String email)
+  {
+    this.email = email;
+  }
+
+  public String getId()
+  {
+    return id;
+  }
+
+  public void setId(String id)
+  {
+    this.id = id;
+  }
 
   public AddressDao getAddress()
   {
