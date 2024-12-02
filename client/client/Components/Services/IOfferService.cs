4c4f06c952ad784f11f8f2d5513e354b1bca5782
Jan Kalina <teksas77@gmail.com>
2024-12-02 20:21:47 +0100

    Refactoring Blazor to use Web App

---
+++ client/client/Components/Services/IOfferService.cs
@@ -0,0 +1,11 @@
+﻿using client.Pages;
+using client.DTO;
+
+namespace client.Services;
+
+public interface IOfferService
+{
+    public Task<OfferResponseDto> SaveOfferAsync(CreateOfferRequestDto createOfferRequestDto);
+    public Task<List<ShortOfferResponseDto>> GetOffersAsync();
+    public Task<OfferResponseDto> GetOfferByIdAsync(string id);
+}
---
+++ client/client/Components/Services/IOrderService.cs
@@ -0,0 +1,9 @@
+﻿using client.Pages;
+using client.DTO;
+
+namespace client.Services;
+
+public interface IOrderService
+{
+    public Task<OrderResponseDto> PlaceOrderAsync(OrderRequestDto orderRequestDto);
+}
