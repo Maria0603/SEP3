fea744a38b923a84e530af8a0427ba73fbf196c7
Mariia Moskovko <masha.moskovko@gmail.com>
2024-12-03 12:03:43 +0100

    search bar with FilterDto

---
+++ client/client/DTO/FilterRequestDto.cs
@@ -0,0 +1,17 @@
+﻿namespace client.DTO;
+
+public class FilterRequestDto {
+    public int MinOfferPrice { get; set; }
+    public int MaxOfferPrice { get; set; }
+    public TimeDto PickupTimeStart { get; set; }
+    public TimeDto PickupTimeEnd { get; set; }
+    public List<string>? Categories { get; set; }
+    
+    public CreateOfferRequestDto ToCreateOfferRequestDto() {
+        return new CreateOfferRequestDto {
+            PickupTimeStart = PickupTimeStart,
+            PickupTimeEnd = PickupTimeEnd,
+            Categories = Categories
+        };
+    }
+}
