5d52cde0abcf37a1b6297a3240f73fa2163cc81b
Jan Kalina <teksas77@gmail.com>
2024-12-03 13:58:11 +0100

    Testing

--- client/client/Program.cs
+++ client/client/Program.cs
@@ -10,7 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddHttpClient();
+builder.Services.AddSingleton(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:7109")
+});
 builder.Services.AddScoped<IOfferService, OfferService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
@@ -38,9 +41,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseStatusCodePagesWithRedirects("/404");
 app.UseHttpsRedirection();
-
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
