fbeda8b77ad9fa6819176be1034f75136be8588b
FlorinaMt <florina.mt77@gmail.com>
2024-12-04 09:31:28 +0100

    Switching from custom Date and Time to google.Timestamp and LocalDateTime - registering new converters for database

---
+++ data_server/src/main/java/com/example/data_server/utility/TimestampToStringConverter.java
@@ -0,0 +1,22 @@
+package com.example.data_server.utility;
+
+import com.google.protobuf.Timestamp;
+import org.springframework.core.convert.converter.Converter;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class TimestampToStringConverter implements Converter<Timestamp, String>
+  {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_DATE_TIME;
+
+    @Override public String convert(Timestamp source)
+    {
+      Instant instant = Instant.ofEpochSecond(source.getSeconds(),
+          source.getNanos());
+      ZonedDateTime zonedDateTime = instant.atZone(ZoneId.systemDefault());
+
+      return zonedDateTime.format(FORMATTER);
+    }
+  }
+
