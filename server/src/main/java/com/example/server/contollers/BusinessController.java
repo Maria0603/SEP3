a9b50bc9e52aea59bb3f3e6394618dadf0b4fa37
AleksandraVIA <293687@via.dk>
2024-12-10 09:22:26 +0100

    business dummy

--- server/src/main/java/com/example/server/contollers/BusinessController.java
+++ server/src/main/java/com/example/server/contollers/BusinessController.java
@@ -1,17 +1,14 @@
 package com.example.server.contollers;
 
-
 import com.example.server.dto.business.BusinessResponseDto;
 import com.example.server.services.IBusinessService;
-import com.example.server.services.Implementations.BusinessService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController @RequestMapping("/business") public class BusinessController {
+@RestController @RequestMapping("/businesses") public class BusinessController {
 
 
     private final IBusinessService businessService;
