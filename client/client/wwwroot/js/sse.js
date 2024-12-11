28b1c8450aed34844d35b672bccf78543d16898d
FlorinaMt <florina.mt77@gmail.com>
2024-12-11 12:40:48 +0100

    SSE - client side

---
+++ client/client/wwwroot/js/sse.js
@@ -0,0 +1,18 @@
+﻿export function connectSSE(url, dotnetHelper) {
+    const eventSource = new EventSource(url);
+
+    eventSource.onopen = (event) => {
+        console.log("SSE connection opened", event);
+    };
+
+    eventSource.onmessage = (event) => {
+        console.log("SSE message received:", event.data); // Add debugging
+        dotnetHelper.invokeMethodAsync("ReceiveNotification", event.data);
+    };
+
+    eventSource.onerror = (event) => {
+        console.error("SSE error", event);
+        console.error("EventSource readyState:", eventSource.readyState);
+        eventSource.close();
+    };
+}
