de437e1087adb2bd8f08fb66501f11120b6ad316
Martman954 <matej.palas954@gmail.com>
2024-12-12 09:24:53 +0100

    Merged sucessfully

--- client/client/Services/IOfferService.cs
+++ client/client/Services/IOfferService.cs
@@ -9,5 +9,6 @@ public interface IOfferService
     public Task<OfferResponseDto> CreateOfferAsync(CreateOfferRequestDto createOfferRequestDto);
     public Task<List<OfferResponseDto>> GetOffersAsync(FilterRequestDto? filterRequestDto);
     public Task<OfferResponseDto> GetOfferByIdAsync(string id);
+    public Task<List<CategoryDto>> GetCategoriesAsync();
     public Task<List<OfferResponseDto>> GetOffersByBusinessIdAsync(string id);
 }
